9231aa1e469c5075806f0dba15f6648e6859adff
Akira106 <akinyarosu@gmail.com>
2024-09-15 12:03:03 +0900

    キーマップの追加

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -10,8 +10,11 @@ vim.opt.expandtab = true
 -- 末尾、先頭から前後の行に移動する
 vim.opt.whichwrap = 'b,s,h,l,[,],<,>,~'
 
--- jjでescapeする
+-- jkでescapeする
 vim.keymap.set('i', 'jk', '<ESC>', { noremap = true, silent = true })
+
+-- fdで削除結果をレジスタに入れない
+vim.keymap.set('n', 'fd', '"_d', { noremap = true, silent = true })
 
 -- 80文字目以降で色を変える
 vim.cmd 'highlight ColorColumn ctermbg=235'
